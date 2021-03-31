3507c1edd633012404c30bfcd8817fca2e428f20
Lucie GOMES <c.luciegomes@gmail.com>
2021-03-31 08:19:15 +0200

    modèle article
    renomage d'un attribut

--- models/article.js
+++ models/article.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const articleSchema = new Schema({
-    numArticle: Number,
+    reference: Number,
+
     designation: {
         type: String,
         required: true
